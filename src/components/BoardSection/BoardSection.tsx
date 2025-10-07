2e68fd54a1369aa34dd7cb765aa229710b33e392
taegyung <taegyung.e@gmail.com>
2025-10-07 21:55:13 +0900

    style: 하단 레이아웃 퍼블리싱

---
+++ src/components/BoardSection/BoardSection.tsx
@@ -0,0 +1,9 @@
+import TodoSection from "../TodoSection/TodoSection";
+
+export default function BoardSection() {
+  return (
+    <section className="flex gap-6 grow-1 p-6 overflow-hidden">
+      <TodoSection />
+    </section>
+  );
+}
