6bedea38c35d850c70f88fb3cb861fa9578c8a63
taegyung <taegyung.e@gmail.com>
2025-10-07 21:53:38 +0900

    feat: 검색바 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import BoardSection from "./components/BoardSection/BoardSection";
+import SearchSection from "./components/SearchSection/SearchSection";
+
 function App() {
-  return <>안녕하세요!</>;
+  return (
+    <main className="flex flex-col h-screen overflow-hidden">
+      <SearchSection />
+      <BoardSection />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/SearchSection/SearchSection.tsx
@@ -0,0 +1,20 @@
+import { IoSearch } from "react-icons/io5";
+
+export default function SearchSection() {
+  return (
+    <section className="flex justify-center items-center shrink-0 p-6 bg-black/10 backdrop-blur-xl">
+      <form
+        action="https://www.google.com/search"
+        className="flex items-center gap-2 w-full max-w-[600px] py-2 px-3 bg-white rounded-full"
+      >
+        <IoSearch className="text-2xl" />
+        <input
+          type="text"
+          name="q"
+          placeholder="검색어를 입력해주세요."
+          className="block w-full h-8 focus:outline-0"
+        />
+      </form>
+    </section>
+  );
+}
